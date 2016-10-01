69ec868097de5655f7a719d33f82ca29abfddff7
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2016-10-02 03:55:43 +0800

    Clear route and edit cap file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "zine#index"
-  resources :zine
-  resources :authors, :only => [:new, :create]
   resources :articles, :only => [:show]
 end
